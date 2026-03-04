87d7128a24ad4035b600c4921c554e573b43b085
Adam Rushford <177971875+Autodidac@users.noreply.github.com>
2026-03-04 05:58:45 -0500

    Refactor comment stripping lexer states and add literal edge tests

--- WinClassicGuiEditor/src/importparser_tests.cpp
+++ WinClassicGuiEditor/src/importparser_tests.cpp
@@ -35,10 +35,16 @@ HWND hSlider = CreateWindowExW(0, L"msctls_trackbar", L"", WS_CHILD | WS_VISIBLE
     10, 260, 120, 30, hwndParent, (HMENU)505, hInst, nullptr);
 HWND hCustomClass = CreateWindowExW(0, L"MyButtonClass", L"Custom", WS_CHILD | WS_VISIBLE,
     10, 300, 120, 30, hwndParent, (HMENU)606, hInst, nullptr);
+HWND hUrlText = CreateWindowExW(0, L"STATIC", L"https://example.com/a//b", WS_CHILD | WS_VISIBLE,
+    10, 340, 180, 30, hwndParent, (HMENU)707, hInst, nullptr);
+HWND hSlashClass = CreateWindowExW(0, L"Custom/*Class//Name", L"Class Slash Test", WS_CHILD | WS_VISIBLE,
+    10, 380, 180, 30, hwndParent, (HMENU)808, hInst, nullptr);
+HWND hStyleLiteral = CreateWindowExW(0, L"BUTTON", L"Style // literal", L"WS_CHILD/*|*/WS_VISIBLE//BS_PUSHBUTTON",
+    10, 420, 180, 30, hwndParent, (HMENU)909, hInst, nullptr);
     )";
 
     auto controls = parse_controls_from_code(code);
-    assert(controls.size() == 8);
+    assert(controls.size() == 11);
 
     assert(controls[0].className == L"STATIC");
     assert(controls[0].text == L"Wide Label");
@@ -63,6 +69,18 @@ HWND hCustomClass = CreateWindowExW(0, L"MyButtonClass", L"Custom", WS_CHILD | W
     assert(controls[7].className == L"MyButtonClass");
     assert(controls[7].text == L"Custom");
     assert(controls[7].id == 606);
+
+    assert(controls[8].className == L"STATIC");
+    assert(controls[8].text == L"https://example.com/a//b");
+    assert(controls[8].id == 707);
+
+    assert(controls[9].className == L"Custom/*Class//Name");
+    assert(controls[9].text == L"Class Slash Test");
+    assert(controls[9].id == 808);
+
+    assert(controls[10].className == L"BUTTON");
+    assert(controls[10].text == L"Style // literal");
+    assert(controls[10].id == 909);
 
     assert(win32_ui_editor::model::ExportClassName(controls[7]) == L"MyButtonClass");
 
